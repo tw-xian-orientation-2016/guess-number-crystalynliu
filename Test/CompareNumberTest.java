b790652f75b4861a9354683350e19c693267887d
crystalynliu <crystalynliu@163.com>
2016-03-04 10:58:47 +0800

    modify test structure to add fixtures

--- Test/CompareNumberTest.java
+++ Test/CompareNumberTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -5,23 +6,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CompareNumberTest {
 
+    CompareNumber compare;
+    @Before
+    public void setUp(){
+        compare = new CompareNumber();
+    }
+    
     @Test
     public void should_return_0A0B_when_input_different_numbers(){
-        CompareNumber compare = new CompareNumber();
         String result = compare.compareNumber("1234","5678");
         assertThat(result,is("0A0B"));
     }
 
     @Test
     public void should_return_0A4B_when_input_different_position_numbers(){
-        CompareNumber compare = new CompareNumber();
         String result = compare.compareNumber("1234","4321");
         assertThat(result,is("0A4B"));
     }
 
     @Test
     public void should_return_4A0B_when_input_equel_numbers(){
-        CompareNumber compare = new CompareNumber();
         String result = compare.compareNumber("1234","1234");
         assertThat(result,is("4A0B"));
     }
