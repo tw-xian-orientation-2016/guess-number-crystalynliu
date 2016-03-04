5b0509da683f32705b220cc028c05df0de5aa096
crystalynliu <crystalynliu@163.com>
2016-03-04 10:28:21 +0800

    add one test case to CompareNumberTest

--- Test/CompareNumberTest.java
+++ Test/CompareNumberTest.java
@@ -1,5 +1,17 @@
-/**
- * Created by crystalynliu on 16-3-4.
- */
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CompareNumberTest {
+
+    @Test
+    public void should_return_0A0B_when_input_different_numbers(){
+        CompareNumber compare = new CompareNumber();
+        String result = compare.compareNumber("1234","5678");
+        assertThat(result,is("0A0B"));
+    }
+
+
+
 }
