b8e1443465f3aac9ae998135cb4b00147e6e07d7
crystalynliu <crystalynliu@163.com>
2016-03-04 14:24:44 +0800

    add two test case to AnswerGenerator

--- Test/AnswerGeneratorTest.java
+++ Test/AnswerGeneratorTest.java
@@ -1,2 +1,30 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AnswerGeneratorTest {
+    @Test
+    public void return_1234_when_input_1_2_3_4_5() {
+        Random random = mock(Random.class);
+
+        when(random.nextInt(10)).thenReturn(1, 2, 3, 4, 5);
+
+        AnswerGenerator answer = new AnswerGenerator(random);
+        assertThat(answer.generator(), is("1234"));
+    }
+
+    @Test
+    public void return_1234_when_input_1_2_2_4_5() {
+        Random random = mock(Random.class);
+
+        when(random.nextInt(10)).thenReturn(1, 2, 2, 4, 5);
+
+        AnswerGenerator answer = new AnswerGenerator(random);
+        assertThat(answer.generator(), is("1245"));
+    }
 }
