8e38e7a1ff935627093fb1f4f5f0cc39987e67e6
crystalynliu <crystalynliu@163.com>
2016-03-04 11:11:45 +0800

    refactor compareNumber

--- src/CompareNumber.java
+++ src/CompareNumber.java
@@ -1,16 +1,13 @@
 
 public class CompareNumber {
     public String compareNumber(String answer, String input) {
-        String[] numbers = input.split("");
         int A = 0, B = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            int index = answer.indexOf(numbers[i]);
-            if ( index != -1) {
-                if(i == index){
-                    A++;
-                }else {
-                    B++;
-                }
+        for (int i = 0; i < input.length(); i++) {
+            int index = answer.indexOf(input.charAt(i));
+            if (index == i) {
+                A++;
+            } else if (index != -1) {
+                B++;
             }
         }
         return A + "A" + B + "B";
