761f7faab77a812f9f58eeaf2b24056a06294d66
crystalynliu <crystalynliu@163.com>
2016-03-04 10:42:03 +0800

    add second test

--- Test/CompareNumberTest.java
+++ Test/CompareNumberTest.java
@@ -12,6 +12,13 @@ public class CompareNumberTest {
         assertThat(result,is("0A0B"));
     }
 
+    @Test
+    public void should_return_0A4B_when_input_different_position_numbers(){
+        CompareNumber compare = new CompareNumber();
+        String result = compare.compareNumber("1234","4321");
+        assertThat(result,is("0A4B"));
+    }
+
 
 
 }
