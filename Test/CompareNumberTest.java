c8da153908f712dc7708552175d9d746dda50b6e
crystalynliu <crystalynliu@163.com>
2016-03-04 10:49:40 +0800

    add third test

--- Test/CompareNumberTest.java
+++ Test/CompareNumberTest.java
@@ -19,6 +19,11 @@ public class CompareNumberTest {
         assertThat(result,is("0A4B"));
     }
 
-
+    @Test
+    public void should_return_4A0B_when_input_equel_numbers(){
+        CompareNumber compare = new CompareNumber();
+        String result = compare.compareNumber("1234","1234");
+        assertThat(result,is("4A0B"));
+    }
 
 }
